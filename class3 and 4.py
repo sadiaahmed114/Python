9d0fdce87fc4af02bfd6297abb2c59825a973056
sadiaahmed114 <sadiaahmed26373@gmail.com>
2024-05-16 19:12:46 -0700

    all

---
+++ class3 and 4.py
@@ -0,0 +1,20 @@
+#  In Third class ...... we learn operators :
+
+# Python divides the operators in the following groups:
+
+# Arithmetic operators
+# Assignment operators
+# Comparison operators .....most important 
+# Logical operators
+# Identity operators
+# Membership operators
+# Presedence order
+
+
+# Class 4 
+# List , indexing , slicing and list method
+
+name1: str = "sadia "
+name2: str = "ayan "
+name3: str = "shaheer"
+
