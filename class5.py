b132a0a91229aceaedeabf7b20764cb78c61e297
sadiaahmed114 <sadiaahmed26373@gmail.com>
2024-05-17 01:05:51 -0700

    First

---
+++ class5.py
@@ -0,0 +1,8 @@
+# Tuples
+
+data_base : list[tuple[str,str]] =[("qasim",'123'),
+                                   ("ali",'345'),
+                                   ("ahmed",'12567')]
+
+for row in data_base:
+    print(row)
