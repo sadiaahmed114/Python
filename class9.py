f2beadd2df5a65151c693007b8a1c67d802eff1f
sadiaahmed114 <sadiaahmed26373@gmail.com>
2024-06-08 02:16:04 -0700

    Initisl

--- class9.py
+++ class9.py
@@ -1,25 +1,8 @@
+import sys
 
-# 📜 *In This Video:*
-# We cover key topics to help you master Python programming:
-# - **While Loops**: Learn to use while loops for repeated execution as long as a condition is true.
-# - **For Loops**: Discover the power of for loops for iterating over sequences and other iterable objects.
-# - **Control Statements**: Understand how to use `break`, `continue`, and `pass` to control the flow of your loops.
-# - **Input Function**: Get hands-on experience with the `input()` function for gathering user input.
-# - **Command Line Arguments**: Explore how to use `sys.argv` for handling command line arguments in your scripts.
+print("Line1")
+print("Line2")
 
-# 🛠️ *Tools and Resources:*
-# - Python Official Documentation: https://docs.python.org/3/
-# - VS Code: https://code.visualstudio.com/
-# - Github: https://github.com/panaverse/learn-mo...
+print(type(sys.argv))
+print(sys.argv)
 
-
-
-# 📖 *References:*
-# - Python Loops: https://docs.python.org/3/tutorial/co...
-# - Python Input and Output: https://docs.python.org/3/tutorial/in...
-
-name: str = input("what is your name? : \t")
-
-print(type(name))
-
-print(f"Welcome dear User Mr/Mrs {name}!")
