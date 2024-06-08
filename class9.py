54633a87c2e0d81033c6307569829e75782ca2b4
sadiaahmed114 <sadiaahmed26373@gmail.com>
2024-06-07 20:10:55 -0700

    Initial

---
+++ class9.py
@@ -0,0 +1,19 @@
+
+# 📜 *In This Video:*
+# We cover key topics to help you master Python programming:
+# - **While Loops**: Learn to use while loops for repeated execution as long as a condition is true.
+# - **For Loops**: Discover the power of for loops for iterating over sequences and other iterable objects.
+# - **Control Statements**: Understand how to use `break`, `continue`, and `pass` to control the flow of your loops.
+# - **Input Function**: Get hands-on experience with the `input()` function for gathering user input.
+# - **Command Line Arguments**: Explore how to use `sys.argv` for handling command line arguments in your scripts.
+
+# 🛠️ *Tools and Resources:*
+# - Python Official Documentation: https://docs.python.org/3/
+# - VS Code: https://code.visualstudio.com/
+# - Github: https://github.com/panaverse/learn-mo...
+
+
+
+# 📖 *References:*
+# - Python Loops: https://docs.python.org/3/tutorial/co...
+# - Python Input and Output: https://docs.python.org/3/tutorial/in...
