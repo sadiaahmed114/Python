c0ec3ac8d2e3dd52cd7b954953b40d8a9ae8780e
sadiaahmed114 <sadiaahmed26373@gmail.com>
2024-06-09 16:36:00 -0700

    Initial

--- class9 and 10.py
+++ class9 and 10.py
@@ -1,8 +1,23 @@
-import sys
+# import sys
 
-print("Line1")
-print("Line2")
+# print("Line1")
+# print("Line2")
 
-print(type(sys.argv))
-print(sys.argv)
+# print(type(sys.argv))
+# print(sys.argv)
+
+from typing import Callable
+
+def my_decorator(func: Callable[[int], None]) -> Callable[[int], None]:
+    def wrapper(num1: int) -> None:
+        print("Something is happening before the function is called.")
+        func(num1)
+        print("Something is happening after the function is called.")
+    return wrapper
+
+@my_decorator
+def say_hello(num1: int) -> None:
+    print(num1)
+
+say_hello(100)
 
